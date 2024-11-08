6ca705c466467e7174c359df9dac89c8bfd28e9e
akram04O <96025201+akram04O@users.noreply.github.com>
2024-11-08 17:13:37 +0100

    adding new project(not complete )

---
+++ loginRegister page/server/app.js
@@ -0,0 +1,50 @@
+import express from 'express'
+import bcrypt from 'bcrypt'
+import cors from 'cors'
+
+const app = express()
+const port = 3000;
+
+const users = [];
+
+app.use(cors());
+app.use(express.json())
+
+app.post('/register', async (req, res) =>{
+    try {
+        const {username, email, password} = req.body;
+        // check if user already exists
+        const existingUser = users.find((data) => data.username === username && data.email === email);
+        if(existingUser){
+            return res.status(400).send({message: "Username or email already exists"})
+        }
+        // hash password
+        const hashedPassword = await bcrypt.hash(password, 10);
+        users.push({username, email, password: hashedPassword});
+        res.status(201).send({message: "User registered successfully"})
+    } catch (error) {
+        res.status(500).send({message: error.message})
+    }
+})
+
+app.post('/login', async (req, res) =>{
+    try {
+        const {email, password} = req.body;
+        // check if user not exists
+        const existingUser = users.find((data) => data.email === email);
+        if(!existingUser){
+            return res.status(404).send({message: "Invalid email or password"})
+        }
+        // check if password is correct
+        const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
+        if(!isPasswordCorrect){
+            return res.status(400).send({message: "Invalid email or password"})
+        }
+        res.status(200).send({message: "Login successful"})
+    } catch (error) {
+        res.status(500).send({message: error.message})
+    }
+})
+app.listen(port, () => {
+    console.log(`app listening on port ${port}`)
+})
